75338d62de677a22f6142d10ed0a81dc48c7ec38
Divya263 <divya.bandarupalli8@gmail.com>
2022-09-20 17:41:56 +0530

    sept20

---
+++ MVCDay3Assignment2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCDay3Assignment2.MvcApplication" Language="C#" %>
---
+++ MVCDay3Assignment2/Models/DepartmentEmployeeContext.cs
@@ -0,0 +1,20 @@
+﻿using MVCDay3Assignment2.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCDay3Assignment2.Models
+{
+    public class DepartmentEmployeeContext: DbContext
+    {
+        public DepartmentEmployeeContext():base("MyConnection")
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+    }
+}
